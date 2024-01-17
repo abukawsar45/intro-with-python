44937aec0324103a3cb08f62ac8a37dfd0e7fcfe
abukawsar45 <kawsar1146236@gmail.com>
2024-01-17 21:42:05 +0600

    add discount system

---
+++ chease.py
@@ -0,0 +1,11 @@
+# print('Assalamualaikum')
+
+product = 1150
+
+if(product>1000):
+  discount=(product/100)*15
+  priceWithoutDiscount=product - discount
+  print(priceWithoutDiscount)
+
+else:
+    print(product)
